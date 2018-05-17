dbae02e81194e39ac6ffbcdcd779f0eac3372456
tk <tobse_tobine@freenet.de>
2018-05-17 12:05:30 +0200

    [DEV] Jenkinsfile + scripts

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,3 +1,3 @@
+node app.js | for /F "tokens=1,2" %%i in ('tasklist /FI "IMAGENAME eq node.exe" /fo table /nh') do echo %%j > .pidfile
 
-node app.js | for /F "tokens=1,2" %i in ('tasklist /FI "IMAGENAME eq node.exe" /fo table /nh') do echo %j > .pidfile
 
